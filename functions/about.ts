2d37cfaad04f96fa94447c3edaabc377e2c1bafc
shenghai92 <zhhsheng9798@gmail.com>
2026-06-24 14:10:23 +0800

    fix(routing): remove broken static page overrides

--- functions/about.ts
+++
@@ -1,5 +0,0 @@
-import { serveStaticPage } from "./_static";
-
-export function onRequest(context: Parameters<typeof serveStaticPage>[0]) {
-  return serveStaticPage(context, "/about/index.html");
-}
